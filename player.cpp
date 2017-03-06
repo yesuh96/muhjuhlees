33af44897f61c71e4721b988fc92243d6afd5a2e
lhyejoon <hlee5@caltech.edu>
2017-03-05 22:41:39 -0800

    initialized board in player class constructor

--- player.cpp
+++ player.cpp
@@ -8,7 +8,7 @@
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
-
+    Board b = Board();
     /*
      * TODO: Do any initialization you need to do here (setting up the board,
      * precalculating things, etc.) However, remember that you will only have
