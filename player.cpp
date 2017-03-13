df6424c4871208b8c5ab7f18c8900fb8ff9b6a49
lhyejoon <hlee5@caltech.edu>
2017-03-13 05:28:23 -0700

    negascout pseudocode

--- player.cpp
+++ player.cpp
@@ -44,6 +44,33 @@ Player::~Player() {
  */
  
 
+/*
+function pvs(node, depth, α, β, color)
+    if (node is a terminal node or depth = 0)
+    {
+        return (color × the heuristic value of node)
+    }
+    for (each child of node)
+    {
+        if child is not first child
+        {
+            score := -pvs(child, depth-1, -α-1, -α, -color)
+            if (α < score < β)
+            {
+                score := -pvs(child, depth-1, -β, -score, -color)
+            }
+        else
+        {
+            score := -pvs(child, depth-1, -β, -α, -color)
+        }
+        α := max(α, score)
+        if (α ≥ β)
+        {
+            break
+        }
+    return α
+    */
+
 Move *Player::doMove(Move *opponentsMove, int msLeft) {
     Board *copy;
     Move *m = new Move(0, 0);
